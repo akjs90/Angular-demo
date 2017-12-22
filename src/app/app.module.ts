6853256765042e68e6ef7d7e54500913347eb648
Akshay Jadhav <aksj90@gmail.com>
2017-12-22 17:24:41 +0530

    Routing: Setting Up Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   ],
   imports: [
   	FormsModule,
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule 
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
